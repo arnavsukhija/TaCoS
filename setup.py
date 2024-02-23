5204c88afc5db1747d4d6ca2fe46e5038f17c9c4
lenarttreven <lenart.treven44@gmail.com>
2024-02-23 13:51:20 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@
 from setuptools import setup, find_packages
 
 required = [
+    'jax>=0.4.13',
+    'brax',
+    'jaxtyping>=0.2.20',
+    'matplotlib>=3.5.1',
     'mbpo @ git+https://github.com/lasgroup/Model-based-policy-optimizers.git',
 ]
 
