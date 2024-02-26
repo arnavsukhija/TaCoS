30e0d14e36a62a8c1c0b1e5d83851c72c54f7f5f
lenarttreven <lenart.treven44@gmail.com>
2024-02-26 14:49:57 +0100

    add plotting of total rewards vs number of switches for the pendulum system, task swing up the pendulum

---
+++ experiments/pedulum_swingup_bounded_switches/analysis/plot_performance_vs_num_switches.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+
+LEGEND_FONT_SIZE = 26
+TITLE_FONT_SIZE = 26
+TABLE_FONT_SIZE = 20
+LABEL_FONT_SIZE = 26
+TICKS_SIZE = 24
+OBSERVATION_SIZE = 300
+
+plt.rc('text', usetex=True)
+plt.rc('text.latex', preamble=
+r'\usepackage{amsmath}'
+r'\usepackage{bm}'
+r'\def\vx{{\bm{x}}}'
+r'\def\vf{{\bm{f}}}')
+
+mpl.rcParams['xtick.labelsize'] = TICKS_SIZE
+mpl.rcParams['ytick.labelsize'] = TICKS_SIZE
+
+data = pd.read_csv('data/wandb_data.csv')
+data_bounded_switches = data.loc[data['wrapper'] == True]
+data_repeated_actions = data.loc[data['wrapper'] == False]
+
+# Prepare data for data_bounded_switches
+grouped_bounded_switches = data_bounded_switches.groupby('num_switches')['results/total_reward'].agg(['mean', 'std'])
+grouped_bounded_switches = grouped_bounded_switches.reset_index()
+
+xs_bounded_switches = np.array(grouped_bounded_switches['num_switches'])
+ys_bounded_switches_mean = np.array(grouped_bounded_switches['mean'])
+ys_bounded_switches_std = np.array(grouped_bounded_switches['std'])
+
+# Prepare data for data_repeated_actions
+grouped_repeated_actions = data_repeated_actions.groupby('results/num_actions')['results/total_reward'].agg(
+    ['mean', 'std'])
+grouped_repeated_actions = grouped_repeated_actions.reset_index()
+
+xs_repeated_actions = np.array(grouped_repeated_actions['results/num_actions'])
+ys_repeated_actions_mean = np.array(grouped_repeated_actions['mean'])
+ys_repeated_actions_std = np.array(grouped_repeated_actions['std'])
+
+# Plotting
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
+
+ax.plot(xs_bounded_switches, ys_bounded_switches_mean, label='Bounded switches')
+ax.fill_between(xs_bounded_switches,
+                ys_bounded_switches_mean - 2 * ys_bounded_switches_std,
+                ys_bounded_switches_mean + 2 * ys_bounded_switches_std,
+                alpha=0.2)
+
+ax.plot(xs_repeated_actions, ys_repeated_actions_mean, label='Repeated actions')
+ax.fill_between(xs_repeated_actions,
+                ys_repeated_actions_mean - 2 * ys_repeated_actions_std,
+                ys_repeated_actions_mean + 2 * ys_repeated_actions_std,
+                alpha=0.2)
+
+ax.set_xlabel('Number of applied actions', fontsize=LABEL_FONT_SIZE)
+ax.set_ylabel('Total reward', fontsize=LABEL_FONT_SIZE)
+ax.legend(fontsize=LEGEND_FONT_SIZE, loc='lower right')
+plt.tight_layout()
+plt.show()
