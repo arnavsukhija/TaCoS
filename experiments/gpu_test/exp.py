c5bbdd4e4dce7617216f801c4d9fd86dd53a3418
lenarttreven <lenart.treven44@gmail.com>
2024-02-27 16:05:36 +0100

    prepare exp of envs for gpu

--- experiments/gpu_test/exp.py
+++ experiments/gpu_test/exp.py
@@ -1,50 +1,78 @@
 import argparse
-import functools
+import datetime
 from datetime import datetime
+import imageio
 
 import jax
+import jax.random as jr
 import matplotlib.pyplot as plt
+import mediapy
 import wandb
 from brax import envs
-from brax.training.agents.sac import train as sac
+from jax.nn import swish
+from mbpo.optimizers.policy_optimizers.sac.sac_brax_env import SAC
+import jax.tree_util as jtu
+import jax.numpy as jnp
+
+ENTITY = 'trevenl'
 
 
-def experiment():
-    wandb.init(project="WhenToControlDebug",
-               dir='/cluster/scratch/trevenl',
-               )
-
-    devices = jax.devices()
-    wandb.log({'devices': str(jax.devices())})
-
-    env_name = 'inverted_pendulum'  # @param ['ant', 'halfcheetah', 'hopper', 'humanoid', 'humanoidstandup',
-                                            # 'inverted_pendulum', 'inverted_double_pendulum', 'pusher',
-                                            # 'reacher', 'walker2d']
-    backend = 'generalized'  # @param ['generalized', 'positional', 'spring']
-
+def experiment(env_name: str = 'inverted_pendulum',
+               backend: str = 'generalized',
+               project_name: str = 'GPUSpeedTest',
+               num_timesteps: int = 1_000_000):
+    assert env_name in ['ant', 'halfcheetah', 'hopper', 'humanoid', 'humanoidstandup', 'inverted_pendulum',
+                        'inverted_double_pendulum', 'pusher', 'reacher', 'walker2d']
+    assert backend in ['generalized', 'positional', 'spring']
     env = envs.get_environment(env_name=env_name,
                                backend=backend)
 
-    state = jax.jit(env.reset)(rng=jax.random.PRNGKey(seed=0))
+    action_repeat = 1
+    episode_length = 1000
+    discount_factor = 0.99
 
-    # We determined some reasonable hyperparameters offline and share them here.
-    train_fn = {
-        'inverted_pendulum': functools.partial(sac.train,
-                                               num_timesteps=100_000,
-                                               num_evals=20,
-                                               reward_scaling=5,
-                                               episode_length=200,
-                                               normalize_observations=True,
-                                               action_repeat=1,
-                                               discounting=0.997,
-                                               learning_rate=3e-4,
-                                               num_envs=16,
-                                               batch_size=32,
-                                               grad_updates_per_step=8,
-                                               max_replay_size=10 ** 5,
-                                               min_replay_size=10 ** 4,
-                                               seed=1),
-    }[env_name]
+    config = dict(env_name=env_name,
+                  backend=backend)
+
+    wandb.init(
+        project=project_name,
+        dir='/cluster/scratch/' + ENTITY,
+        config=config,
+    )
+
+    optimizer = SAC(
+        environment=env,
+        num_timesteps=num_timesteps,
+        episode_length=episode_length,
+        action_repeat=action_repeat,
+        num_env_steps_between_updates=20,
+        num_envs=32,
+        num_eval_envs=4,
+        lr_alpha=3e-4,
+        lr_policy=3e-4,
+        lr_q=3e-4,
+        wd_alpha=0.,
+        wd_policy=0.,
+        wd_q=0.,
+        max_grad_norm=1e5,
+        discounting=discount_factor,
+        batch_size=32,
+        num_evals=20,
+        normalize_observations=True,
+        reward_scaling=1.,
+        tau=0.005,
+        min_replay_size=10 ** 4,
+        max_replay_size=10 ** 5,
+        grad_updates_per_step=20 * 32,  # should be num_envs * num_env_steps_between_updates
+        deterministic_eval=True,
+        init_log_alpha=0.,
+        policy_hidden_layer_sizes=(32,) * 5,
+        policy_activation=swish,
+        critic_hidden_layer_sizes=(128,) * 4,
+        critic_activation=swish,
+        wandb_logging=True,
+        return_best_model=True,
+    )
 
     xdata, ydata = [], []
     times = [datetime.now()]
@@ -61,26 +89,65 @@ def experiment():
         plt.show()
 
     print('Before inference')
-    make_inference_fn, params, _ = train_fn(environment=env, progress_fn=progress)
+    policy_params, metrics = optimizer.run_training(key=jr.PRNGKey(0), progress_fn=progress)
     print('After inference')
+
+    # Now we plot the evolution
+    pseudo_policy = optimizer.make_policy(policy_params, deterministic=True)
+
+    def policy(obs):
+        return pseudo_policy(obs, key_sample=jr.PRNGKey(0))
+
+    @jax.jit
+    def step(state, _):
+        u = policy(state.obs)[0]
+        print('Step')
+        print(f'Time to go {u[-1]}')
+        next_state = env.step(state, u)
+        return next_state
 
     time_to_jit = times[1] - times[0]
     time_to_train = times[-1] - times[1]
 
-    wandb.log({'time_to_jit': str(time_to_jit),
-               'time_to_train': str(time_to_train)})
-
     print(f'time to jit: {time_to_jit}')
     print(f'time to train: {time_to_train}')
 
+    state = env.reset(rng=jr.PRNGKey(0))
+    trajectory = []
+    trajectory.append(state)
+    rewards = []
+    for i in range(episode_length):
+        state = step(state, None)
+        trajectory.append(state)
+        rewards.append(state.reward)
+
+    total_reward = sum(rewards)
+    wandb.log({'total_reward': total_reward})
+
+    # video_frames = env.render([s.pipeline_state for s in trajectory], camera='track')
+    video_frames = env.render([s.pipeline_state for s in trajectory])
+
+    with imageio.get_writer('video.mp4', fps=int(1 / env.dt)) as writer:
+        for frame in video_frames:
+            writer.append_data(frame)
+
+    wandb.log({"video": wandb.Video("video.mp4", fps=int(1 / env.dt), format="gif")})
     wandb.finish()
 
 
 def main(args):
-    experiment()
+    experiment(env_name=args.env_name,
+               backend=args.backend,
+               project_name=args.project_name,
+               num_timesteps=args.num_timesteps)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--env_name', type=str, default='inverted_pendulum')
+    parser.add_argument('--backend', type=str, default='generalized')
+    parser.add_argument('--project_name', type=str, default='GPUSpeedTest')
+    parser.add_argument('--num_timesteps', type=int, default=40_000)
+
     args = parser.parse_args()
     main(args)
---
+++ experiments/model_free_single_gpu/launcher.py
@@ -0,0 +1,34 @@
+import exp
+from experiments.util import generate_run_commands, generate_base_command, dict_permutations
+
+PROJECT_NAME = 'SingleGPUTestFeb27_15_44'
+
+general_configs = {
+    'env_name': ['ant', 'halfcheetah', 'hopper', 'humanoid', 'humanoidstandup', 'inverted_pendulum',
+                 'inverted_double_pendulum', 'pusher', 'reacher', 'walker2d'],
+    'backend': ['generalized', 'positional', 'spring'],
+    'project_name': [PROJECT_NAME],
+    'num_timesteps': [1_000_000, ],
+}
+
+
+def main():
+    command_list = []
+
+    flags_combinations = dict_permutations(general_configs)
+    for flags in flags_combinations:
+        cmd = generate_base_command(exp, flags=flags)
+        command_list.append(cmd)
+
+    # submit jobs
+    generate_run_commands(command_list,
+                          num_cpus=1,
+                          num_gpus=1,
+                          mode='euler',
+                          duration='3:59:00',
+                          prompt=True,
+                          mem=16000)
+
+
+if __name__ == '__main__':
+    main()
