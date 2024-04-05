b1d92a4728a04c9ea29784faa00b12f74a0269ca
lenarttreven <lenart.treven44@gmail.com>
2024-04-05 11:59:14 +0200

    plotting of num actions and reward with/without switch cost on the tracking greenhouse challange

--- experiments/noise_influence/analysis/plot_noise_num_actions.py
+++ experiments/noise_influence/analysis/plot_noise_num_actions.py
@@ -5,7 +5,7 @@ import matplotlib as mpl
 from scipy.ndimage import gaussian_filter1d
 
 LEGEND_FONT_SIZE = 26
-TITLE_FONT_SIZE = 26
+TITLE_FONT_SIZE = 30
 TABLE_FONT_SIZE = 20
 LABEL_FONT_SIZE = 26
 TICKS_SIZE = 24
@@ -24,6 +24,8 @@ mpl.rcParams['xtick.labelsize'] = TICKS_SIZE
 mpl.rcParams['ytick.labelsize'] = TICKS_SIZE
 
 data = pd.read_csv('data/greenhouse_data.csv')
+data['results/reward_with_switch_cost'] = data['results/total_reward'] - 0.2 * data['results/num_actions']
+
 data_performance = pd.read_csv('data/noise_influence_performance.csv')
 data_performance = data_performance[data_performance['env_name'] == 'Greenhouse']
 
@@ -55,6 +57,24 @@ xs_total_reward_full_control = np.array(grouped_total_reward_full_control['scale
 ys_total_rewards_mean_full_control = np.array(grouped_total_reward_full_control['mean'])
 ys_total_reward_std_full_control = np.array(grouped_total_reward_full_control['std'])
 
+# Prepare data for the reward with switch cost
+grouped_reward_with_switch_cost = data.groupby('scale')['results/reward_with_switch_cost'].agg(['mean', 'std'])
+grouped_reward_with_switch_cost = grouped_reward_with_switch_cost.reset_index()
+
+xs_reward_with_switch_cost = np.array(grouped_reward_with_switch_cost['scale'])
+ys_reward_with_switch_cost_mean = np.array(grouped_reward_with_switch_cost['mean'])
+ys_reward_with_switch_cost_std = np.array(grouped_reward_with_switch_cost['std'])
+
+# Smooth to get the trend
+sigma = 3  # Standard deviation for Gaussian kernel
+ys_total_rewards_mean_full_control = gaussian_filter1d(ys_total_rewards_mean_full_control, sigma)
+ys_total_reward_std_full_control = gaussian_filter1d(ys_total_reward_std_full_control, sigma)
+
+
+# Smooth to get the trend
+sigma = 3  # Standard deviation for Gaussian kernel
+ys_reward_with_switch_cost_mean = gaussian_filter1d(ys_reward_with_switch_cost_mean, sigma)
+ys_reward_with_switch_cost_std = gaussian_filter1d(ys_reward_with_switch_cost_std, sigma)
 
 # Smooth to get the trend
 sigma = 3  # Standard deviation for Gaussian kernel
@@ -62,7 +82,7 @@ ys_total_rewards_mean = gaussian_filter1d(ys_total_rewards_mean, sigma)
 ys_total_reward_std = gaussian_filter1d(ys_total_reward_std, sigma)
 
 # Plotting
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(20, 8))
 
 ax[0].plot(xs_num_actions, ys_num_actions_mean)
 ax[0].fill_between(xs_num_actions,
@@ -78,18 +98,54 @@ ax[1].fill_between(xs_total_reward,
                    ys_total_rewards_mean + ys_total_reward_std / np.sqrt(NUM_SAMPLES_PER_SEED),
                    alpha=0.2)
 
-ax[1].plot(xs_total_reward_full_control, ys_total_rewards_mean_full_control, label="Control per integration \n step [200 actions]")
+ax[1].plot(xs_total_reward_full_control, ys_total_rewards_mean_full_control,
+           label="Control per integration step [200 actions]")
 ax[1].fill_between(xs_total_reward_full_control,
-                   ys_total_rewards_mean_full_control - ys_total_reward_std_full_control / np.sqrt(NUM_SAMPLES_PER_SEED),
-                   ys_total_rewards_mean_full_control + ys_total_reward_std_full_control / np.sqrt(NUM_SAMPLES_PER_SEED),
+                   ys_total_rewards_mean_full_control - ys_total_reward_std_full_control / np.sqrt(
+                       NUM_SAMPLES_PER_SEED),
+                   ys_total_rewards_mean_full_control + ys_total_reward_std_full_control / np.sqrt(
+                       NUM_SAMPLES_PER_SEED),
                    alpha=0.2)
-ax[1].legend(fontsize=LEGEND_FONT_SIZE, loc='lower left')
+# ax[1].legend(fontsize=LEGEND_FONT_SIZE, loc='lower left')
 ax[1].set_xlabel(r'Noise magnitude: $x \times $ DEFAULT_STD', fontsize=LABEL_FONT_SIZE)
 ax[1].set_ylabel('Reward [Without Switch Cost]', fontsize=LABEL_FONT_SIZE)
 
 
+ax[2].plot(xs_num_actions, ys_reward_with_switch_cost_mean, label='Switch cost control')
+ax[2].fill_between(xs_total_reward,
+                   ys_reward_with_switch_cost_mean - ys_reward_with_switch_cost_std / np.sqrt(NUM_SAMPLES_PER_SEED),
+                   ys_reward_with_switch_cost_mean + ys_reward_with_switch_cost_std / np.sqrt(NUM_SAMPLES_PER_SEED),
+                   alpha=0.2)
+
+ax[2].plot(xs_total_reward_full_control, ys_total_rewards_mean_full_control - 40,
+           label="Control per integration step [200 actions]")
+ax[2].fill_between(xs_total_reward_full_control,
+                   ys_total_rewards_mean_full_control - 40 - ys_total_reward_std_full_control / np.sqrt(
+                       NUM_SAMPLES_PER_SEED),
+                   ys_total_rewards_mean_full_control - 40 + ys_total_reward_std_full_control / np.sqrt(
+                       NUM_SAMPLES_PER_SEED),
+                   alpha=0.2)
+# ax[2].legend(fontsize=LEGEND_FONT_SIZE, loc='lower left')
+ax[2].set_xlabel(r'Noise magnitude: $x \times $ DEFAULT_STD', fontsize=LABEL_FONT_SIZE)
+ax[2].set_ylabel('Reward [With Switch Cost]', fontsize=LABEL_FONT_SIZE)
+
+handles, labels = [], []
+for axs in ax:
+    for handle, label in zip(*axs.get_legend_handles_labels()):
+        handles.append(handle)
+        labels.append(label)
+by_label = dict(zip(labels, handles))
+
+fig.legend(by_label.values(), by_label.keys(),
+           ncols=2,
+           loc='upper center',
+           bbox_to_anchor=(0.5, 0.89),
+           fontsize=LEGEND_FONT_SIZE,
+           frameon=False)
+
 fig.suptitle('Greenhouse temperature-tracking task [Duration = 200 min], [Switch Cost = 0.2]',
-             fontsize=TITLE_FONT_SIZE)
-plt.tight_layout()
+             fontsize=TITLE_FONT_SIZE,
+             y=0.92)
+fig.tight_layout(rect=[0.0, 0.0, 1, 0.88])
 plt.savefig('greenhouse_switch_cost_varying_noise.pdf')
 plt.show()
