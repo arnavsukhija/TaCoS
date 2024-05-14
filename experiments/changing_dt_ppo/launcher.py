c29c2452d79eebef674f4a0edb9cd32a504d5ae9
lenarttreven <lenart.treven44@gmail.com>
2024-05-14 16:11:02 +0200

    add rccar exp for changing dt

--- experiments/changing_dt_ppo/launcher.py
+++ experiments/changing_dt_ppo/launcher.py
@@ -176,6 +176,30 @@ reacher_switch_cost = {'env_name': ['reacher', ],
 #################### RC Car ####################
 ################################################
 
+rccar_switch_cost = {'env_name': ['rccar', ],
+                     'backend': ['generalized', ],
+                     'project_name': ["RCCARPPOSwitchCostMay14_16_05"],
+                     'num_timesteps': [2_000_000, ],
+                     'episode_time': [4.0, ],
+                     'base_dt_divisor': [1, 2, 5, 10, 25, 50, 80, 100, 150, 200],
+                     'base_discount_factor': [0.9],
+                     'seed': list(range(5)),
+                     'num_envs': [2048],
+                     'num_eval_envs': [32],
+                     'entropy_cost': [1e-2],
+                     'unroll_length': [10],
+                     'num_minibatches': [32],
+                     'num_updates_per_batch': [4],
+                     'batch_size': [1024],
+                     'networks': [0, ],
+                     'reward_scaling': [1.0, ],
+                     'switch_cost_wrapper': [1, ],
+                     'switch_cost': [0.1, ],
+                     'max_time_between_switches': [0.5],
+                     'time_as_part_of_state': [1, ],
+                     'num_final_evals': [1, ]
+                     }
+
 # general_configs = {
 #     'project_name': ["RCCarSwitchCostApr17_14_00"],
 #     'backend': ['generalized', ],
@@ -364,9 +388,10 @@ def main():
     #         flags_combinations = dict_permutations(conf)
     #     else:
     #         flags_combinations += dict_permutations(conf)
-    flags_combinations = dict_permutations(reacher_switch_cost)
-    flags_combinations += dict_permutations(halfcheetah_switch_cost)
-    flags_combinations += dict_permutations(humanoid_switch_cost)
+    flags_combinations = dict_permutations(rccar_switch_cost)
+    # flags_combinations += dict_permutations(reacher_switch_cost)
+    # flags_combinations += dict_permutations(halfcheetah_switch_cost)
+    # flags_combinations += dict_permutations(humanoid_switch_cost)
 
     for flags in flags_combinations:
         cmd = generate_base_command(exp, flags=flags)
