40507ce069f894e237e3109ae853ab37b3eafa58
Arnav Sukhija <arnavsonic@gmail.com>
2025-03-27 18:35:53 +0100

    fixed action delay by adding to info part of state

--- wtc/wrappers/ActionDelayWrapper.py
+++ wtc/wrappers/ActionDelayWrapper.py
@@ -14,6 +14,7 @@ class ActionDelayWrapper(Wrapper):
         # Buffer parameters for action delay buffer
         self.dt = env.dt
         delay_steps = action_delay / self.dt  # calculates the number of delay steps
+        assert(delay_steps >= 1)
         self.buffer_size = int(jnp.ceil(delay_steps)) + 1
         if abs(action_delay % self.dt) < 1e-8:
             self.interp_weights = jnp.array([1.0, 0.0])
@@ -25,24 +26,23 @@ class ActionDelayWrapper(Wrapper):
         "Uses the environments reset function and adds the action_delay buffer to the new state as part of info (easy maintenance), to maintain Markovian property"
         state = self.env.reset(rng)
         action_buffer = jnp.zeros((self.buffer_size, self.action_size))
-        new_obs = {'observation': state.obs, 'action_buffer': action_buffer}
-        return state.replace(obs=new_obs)
+        new_info = {'action_buffer': action_buffer}
+        updated_info = {**state.info, **new_info}
+        return state.replace(info=updated_info)
 
     def step(self, state: State, action: jax.Array) -> State:
         """We take a step with delayed action"""
         # get delayed action (interpolate between two actions if the delay is not a multiple of dt)
-        action_buffer = state.obs['action_buffer']
+        action_buffer = state.info['action_buffer']
         # we reset the original structure of the state so that the base environment can process it easily
-        obs = state.obs['observation']
-        state = state.replace(obs=obs)
         delayed_action = jnp.sum(action_buffer[:2] * self.interp_weights[:, None], axis=0)
         next_state = self.env.step(state, delayed_action)
-        # we derive the new action buffer and transform the state accordingly
-        new_action_buffer = jnp.concatenate([state.action_buffer[1:], action[None]], axis=0)
-        new_obs = {'observation': next_state.obs, 'action_buffer': new_action_buffer}
-        control_penalty = -self.ctrl_diff_weight * jnp.sum((action - state.action_buffer[
-            -1]) ** 2)  #compute control penalty based on the predicted action and the last action in buffer
-        return next_state.replace(obs=new_obs, reward=next_state.reward + control_penalty)
+        # we derive the new action buffer and pass it accordingly
+        new_action_buffer = jnp.concatenate([action_buffer[1:], action[None]], axis=0)
+        new_info = {'action_buffer': new_action_buffer}
+        updated_info = {**next_state.info, **new_info}
+        control_penalty = -self.ctrl_diff_weight * jnp.sum((action - action_buffer[-1]) ** 2)  #compute control penalty based on the predicted action and the last action in buffer
+        return next_state.replace(reward=next_state.reward + control_penalty, info=updated_info)
 
     @property
     def observation_size(self) -> int:
